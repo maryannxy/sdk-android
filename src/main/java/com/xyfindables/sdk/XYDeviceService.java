8233391099047a15e1df4a664ad73736cd74f87d
Arie Trouw <arie.trouw@xyfindit.com>
2017-04-27 02:05:50 +0800

    Service Name Update

--- src/main/java/com/xyfindables/sdk/XYDeviceService.java
+++ src/main/java/com/xyfindables/sdk/XYDeviceService.java
@@ -7,11 +7,10 @@ import java.util.UUID;
  */
 
 public class XYDeviceService {
-
     public static final UUID Control = UUID.fromString("F014ED15-0439-3000-E001-00001001FFFF");
     public static final UUID BasicConfig = UUID.fromString("F014EE00-0439-3000-E001-00001001FFFF");
     public static final UUID ExtendedConfig = UUID.fromString("F014FF00-0439-3000-E001-00001001FFFF");
     public static final UUID CsrOta = UUID.fromString("00001016-D102-11E1-9B23-00025B00A5A5");
-    public static final UUID BatteryStandard = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb");
+    public static final UUID BatteryStandard = UUID.fromString("0000180F-0000-1000-8000-00805F9B34FB");
     public static final UUID ExtendedControl = UUID.fromString("F014AA00-0439-3000-E001-00001001FFFF");
 }
