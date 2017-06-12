ee33bac0e52e0671ececb8c6e9275da8234948ac
Alex McElroy <amcel1288@gmail.com>
2017-06-12 15:39:56 -0700

    add actions for OtaWrite and reboot

---
+++ sdk-android-library/src/main/java/com/xyfindables/sdk/action/XYDeviceActionOtaWrite.java
@@ -0,0 +1,52 @@
+package com.xyfindables.sdk.action;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.util.Log;
+
+import com.xyfindables.sdk.XYDevice;
+import com.xyfindables.sdk.XYDeviceCharacteristic;
+import com.xyfindables.sdk.XYDeviceService;
+
+import java.util.UUID;
+
+/**
+ * Created by alex.mcelroy on 6/12/2017.
+ */
+
+public abstract class XYDeviceActionOtaWrite extends XYDeviceAction {
+
+    private static final String TAG = XYDeviceActionOtaWrite.class.getSimpleName();
+
+    public byte[] value;
+
+    public XYDeviceActionOtaWrite(XYDevice device, byte[] value) {
+        super(device);
+        this.value = value;
+        Log.v(TAG, TAG);
+    }
+
+    @Override
+    public UUID getServiceId() {
+        return XYDeviceService.BasicConfig;
+    }
+
+    @Override
+    public UUID getCharacteristicId() {
+        return XYDeviceCharacteristic.BasicConfigOtaWrite;
+    }
+
+    @Override
+    public boolean statusChanged(int status, BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, boolean success) {
+        Log.v(TAG, "statusChanged:" + status + ":" + success);
+        boolean result = super.statusChanged(status, gatt, characteristic, success);
+        switch (status) {
+            case STATUS_CHARACTERISTIC_FOUND: {
+                characteristic.setValue(value);
+                gatt.writeCharacteristic(characteristic);
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ sdk-android-library/src/main/java/com/xyfindables/sdk/action/XYDeviceActionReboot.java
@@ -0,0 +1,52 @@
+package com.xyfindables.sdk.action;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.util.Log;
+
+import com.xyfindables.sdk.XYDevice;
+import com.xyfindables.sdk.XYDeviceCharacteristic;
+import com.xyfindables.sdk.XYDeviceService;
+
+import java.util.UUID;
+
+/**
+ * Created by alex.mcelroy on 6/12/2017.
+ */
+
+public abstract class XYDeviceActionReboot extends XYDeviceAction {
+
+    private static final String TAG = XYDeviceActionReboot.class.getSimpleName();
+
+    public int value;
+
+    public XYDeviceActionReboot(XYDevice device, int value) {
+        super(device);
+        this.value = value;
+        Log.v(TAG, TAG);
+    }
+
+    @Override
+    public UUID getServiceId() {
+        return XYDeviceService.BasicConfig;
+    }
+
+    @Override
+    public UUID getCharacteristicId() {
+        return XYDeviceCharacteristic.BasicConfigReboot;
+    }
+
+    @Override
+    public boolean statusChanged(int status, BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, boolean success) {
+        Log.v(TAG, "statusChanged:" + status + ":" + success);
+        boolean result = super.statusChanged(status, gatt, characteristic, success);
+        switch (status) {
+            case STATUS_CHARACTERISTIC_FOUND: {
+                characteristic.setValue(value, BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+                gatt.writeCharacteristic(characteristic);
+                break;
+            }
+        }
+        return result;
+    }
+}
