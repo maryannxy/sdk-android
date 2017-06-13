c9f36bbb49a391003b75c5509c1c1088f917cd67
Alex McElroy <amcel1288@gmail.com>
2017-06-13 09:32:01 -0700

    added profile characteristics

---
+++ sdk-android-library/src/main/java/com/xyfindables/sdk/action/XYDeviceActionGetProfile.java
@@ -0,0 +1,51 @@
+package com.xyfindables.sdk.action;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.util.Log;
+
+import com.xyfindables.sdk.XYDevice;
+import com.xyfindables.sdk.XYDeviceCharacteristic;
+import com.xyfindables.sdk.XYDeviceService;
+
+import java.util.UUID;
+
+/**
+ * Created by alex.mcelroy on 6/13/2017.
+ */
+
+public class XYDeviceActionGetProfile extends XYDeviceAction {
+    private static final String TAG = XYDeviceActionGetProfile.class.getSimpleName();
+
+    public int value;
+
+    public XYDeviceActionGetProfile(XYDevice device) {
+        super(device);
+        Log.v(TAG, TAG);
+    }
+
+    @Override
+    public UUID getServiceId() {
+        return XYDeviceService.ExtendedControl;
+    }
+
+    @Override
+    public UUID getCharacteristicId() {
+        return XYDeviceCharacteristic.GpsProfile;
+    }
+
+    @Override
+    public boolean statusChanged(int status, BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, boolean success) {
+        Log.v(TAG, "statusChanged:" + status + ":" + success);
+        boolean result = super.statusChanged(status, gatt, characteristic, success);
+        switch (status) {
+            case STATUS_CHARACTERISTIC_READ:
+                value = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT8, 0);
+                break;
+            case STATUS_CHARACTERISTIC_FOUND:
+                gatt.readCharacteristic(characteristic);
+                break;
+        }
+        return result;
+    }
+}
---
+++ sdk-android-library/src/main/java/com/xyfindables/sdk/action/XYDeviceActionSetProfile.java
@@ -0,0 +1,51 @@
+package com.xyfindables.sdk.action;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.util.Log;
+
+import com.xyfindables.sdk.XYDevice;
+import com.xyfindables.sdk.XYDeviceCharacteristic;
+import com.xyfindables.sdk.XYDeviceService;
+
+import java.util.UUID;
+
+/**
+ * Created by alex.mcelroy on 6/13/2017.
+ */
+
+public class XYDeviceActionSetProfile extends XYDeviceAction {
+    private static final String TAG = XYDeviceActionSetProfile.class.getSimpleName();
+
+    public int value;
+
+    public XYDeviceActionSetProfile(XYDevice device, int value) {
+        super(device);
+        this.value = value;
+        Log.v(TAG, TAG);
+    }
+
+    @Override
+    public UUID getServiceId() {
+        return XYDeviceService.ExtendedControl;
+    }
+
+    @Override
+    public UUID getCharacteristicId() {
+        return XYDeviceCharacteristic.GpsProfile;
+    }
+
+    @Override
+    public boolean statusChanged(int status, BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, boolean success) {
+        Log.v(TAG, "statusChanged:" + status + ":" + success);
+        boolean result = super.statusChanged(status, gatt, characteristic, success);
+        switch (status) {
+            case STATUS_CHARACTERISTIC_FOUND: {
+                characteristic.setValue(value, BluetoothGattCharacteristic.FORMAT_UINT16, 0);
+                gatt.writeCharacteristic(characteristic);
+                break;
+            }
+        }
+        return result;
+    }
+}
